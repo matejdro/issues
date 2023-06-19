175aa2e45839003ef210b0cdaedff175f709a4bb
Matej Drobnic <matej.drobnic@inova.si>
2023-06-19 07:12:09 +0200

    replace AGP with regular JVM project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,40 +1,8 @@
 @Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
-    alias(libs.plugins.com.android.application)
-    alias(libs.plugins.org.jetbrains.kotlin.android)
+    id("java")
+    alias(libs.plugins.org.jetbrains.kotlin.jvm)
     id("demo-plugin")
-}
-
-android {
-    namespace = "com.matejdro.tomlbugdemoandroid"
-    compileSdk = 33
-
-    defaultConfig {
-        applicationId = "com.matejdro.tomlbugdemoandroid"
-        minSdk = 24
-        targetSdk = 33
-        versionCode = 1
-        versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
 }
 
 dependencies {
