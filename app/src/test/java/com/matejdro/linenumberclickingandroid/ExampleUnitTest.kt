5ff0aaf284370d5813b323277bacebc0c13b7b63
Matej Drobnič <matej@matejdro.com>
2023-08-03 14:13:54 +0200

    Commit project

---
+++ app/src/test/java/com/matejdro/linenumberclickingandroid/ExampleUnitTest.kt
@@ -0,0 +1,21 @@
+package com.matejdro.linenumberclickingandroid
+
+import io.kotest.assertions.assertSoftly
+import io.kotest.matchers.shouldBe
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun Demo() {
+        assertSoftly(Unit) {
+            true shouldBe false
+        }
+    }
+}
